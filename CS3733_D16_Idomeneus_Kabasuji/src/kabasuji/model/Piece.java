5afc5d9db894a2c3342365832aab4ae33fde93e6
Ethanprihar <ethanprihar@gmail.com>
2016-04-11 19:56:17 -0400

    wrote the methods and constructor for the piece class

--- CS3733_D16_Idomeneus_Kabasuji/src/kabasuji/model/Piece.java
+++ CS3733_D16_Idomeneus_Kabasuji/src/kabasuji/model/Piece.java
@@ -1,5 +1,46 @@
 package kabasuji.model;
 
-public class Piece {
-
+public class Piece
+{
+	BlankTile[][] tiles;
+	
+	public Piece(BlankTile[][] t)
+	{
+		tiles = t;
+	}
+	
+	public void rotate(boolean left)
+	{
+		flip(left);
+		transpose();
+	}
+	
+	public void flip(boolean vertical)
+	{
+		BlankTile[][] newTiles = new BlankTile[tiles.length][tiles.length];
+		for(int i=0; i<tiles.length; i++)
+		{
+			for(int j=0; j<tiles.length; j++)
+			{
+				if(vertical)
+					newTiles[i][j] = tiles[tiles.length-1-i][j];
+				else
+					newTiles[i][j] = tiles[i][tiles.length-1-j];
+			}
+		}
+		tiles = newTiles;
+	}
+	
+	private void transpose()
+	{
+		BlankTile[][] newTiles = new BlankTile[tiles.length][tiles.length];
+		for(int i=0; i<tiles.length; i++)
+		{
+			for(int j=0; j<tiles.length; j++)
+			{
+				newTiles[i][j] = tiles[tiles.length-1-i][tiles.length-1-j];
+			}
+		}
+		tiles = newTiles;
+	}
 }
