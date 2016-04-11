82dcf4c4c2a4006300552da22e7766dbad4f4a97
Vishal Rathi <vishalkumar95@users.noreply.github.com>
2016-04-10 20:37:23 -0400

    Quick design for the opening screen

--- CS3733_D16_Idomeneus_Kabasuji/src/kabasuji/view/MainMenu.java
+++ CS3733_D16_Idomeneus_Kabasuji/src/kabasuji/view/MainMenu.java
@@ -7,6 +7,13 @@ import javax.swing.JButton;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.SystemColor;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainMenu extends JPanel {
 
@@ -14,33 +21,76 @@ public class MainMenu extends JPanel {
 	 * Create the panel.
 	 */
 	public MainMenu() {
-		setBackground(Color.YELLOW);
+		setBackground(SystemColor.textHighlight);
 		setBorder(new CompoundBorder());
 		
-		JButton btnNewButton = new JButton("New button");
+		JLabel lblKabasuji = new JLabel("Kabasuji");
+		lblKabasuji.setHorizontalAlignment(SwingConstants.CENTER);
+		lblKabasuji.setFont(new Font("Tahoma", Font.BOLD, 73));
 		
-		JButton btnNewButton_1 = new JButton("New button");
+		JLabel lblNewLabel = new JLabel("Odell Dotson");
+		
+		JLabel label = new JLabel("Ethan Prihar");
+		
+		JLabel label_1 = new JLabel("Vishal Rathi");
+		
+		JLabel label_2 = new JLabel("Breanne Happell");
+		
+		JLabel label_3 = new JLabel("yu-Sen Wu");
+		
+		JButton btnNewButton = new JButton("Level Select");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(btnNewButton)
-					.addPreferredGap(ComponentPlacement.RELATED, 252, Short.MAX_VALUE)
-					.addComponent(btnNewButton_1)
-					.addContainerGap())
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(233)
+							.addComponent(lblNewLabel))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(233)
+							.addComponent(label))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(233)
+							.addComponent(label_1))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(233)
+							.addComponent(label_2))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(233)
+							.addComponent(label_3))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(106)
+							.addComponent(lblKabasuji))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(216)
+							.addComponent(btnNewButton)))
+					.addContainerGap(124, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnNewButton)
-						.addComponent(btnNewButton_1))
-					.addContainerGap(266, Short.MAX_VALUE))
+					.addGap(75)
+					.addComponent(lblKabasuji)
+					.addGap(76)
+					.addComponent(lblNewLabel)
+					.addGap(18)
+					.addComponent(label)
+					.addGap(18)
+					.addComponent(label_1)
+					.addGap(18)
+					.addComponent(label_2)
+					.addGap(18)
+					.addComponent(label_3)
+					.addGap(71)
+					.addComponent(btnNewButton)
+					.addContainerGap(126, Short.MAX_VALUE))
 		);
 		setLayout(groupLayout);
 
 	}
-
 }
