2a8b2cb0214b620e8acbed89086661c50f05e4d7
Ethanprihar <ethanprihar@gmail.com>
2016-04-12 18:52:04 -0400

    Reorganized tiles to all be one class

--- CS3733_D16_Idomeneus_Kabasuji/src/kabasuji/model/Tile.java
+++ CS3733_D16_Idomeneus_Kabasuji/src/kabasuji/model/Tile.java
@@ -1,42 +1,61 @@
 package kabasuji.model;
 
-public class Tile extends BlankTile
-{
-	
-	boolean valid; // whether the tile is interactable with
+public class Tile {
+
+	boolean valid; // whether the tile is part of a board or a piece or just extra in the matrix
 	boolean hint;
-	Piece piece;
-	int number; // 0 means no number, 1 - 6 means number
 	int color; // 0 mean no color, 1 - 3 means color
-	
-	public Tile()
-	{
+	int number; // 0 means no number, 1 - 6 means number
+	Piece piece;
+
+	public Tile(boolean h, boolean v, int c, int n) {
 		hint = false;
 		piece = null;
+		color = 0;
+		number = 0;
 	}
-	
-	public boolean isHint()
-	{
-		return hint;
-	}
-	
-	public void setHint(boolean h)
-	{
-		hint = h;
-	}
-	
-	public void toggleHint()
-	{
+
+	public void toggleHint() {
 		hint = !hint;
 	}
-	
-	public Piece getPiece()
-	{
+
+	public boolean isValid() {
+		return valid;
+	}
+
+	public boolean isHint() {
+		return hint;
+	}
+
+	public int getColor() {
+		return color;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public Piece getPiece() {
 		return piece;
 	}
+
+	public void setValid(boolean v) {
+		valid = v;
+	}
+
+	public void setHint(boolean h) {
+		hint = h;
+	}
+
+	public void setColor(int c) {
+		color = c;
+	}
+
+	public void setNumber(int n) {
+		number = n;
+	}
 	
-	public void setPiece(Piece p)
-	{
+	public void setPiece(Piece p) {
 		piece = p;
 	}
 }
