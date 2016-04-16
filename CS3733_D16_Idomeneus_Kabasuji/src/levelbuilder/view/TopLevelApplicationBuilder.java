d704e41267bc651c3158ddc50fdac9709c7cc240
Vishal Rathi <vishalkumar95@users.noreply.github.com>
2016-04-16 16:47:37 -0400

    Update the BuilderMainMenu to reflect the new GUI changes, add the background image for the BuilderTitleScreen, modify the CreateNewLevelBui
    lderController and make appropriate changes to
    TopLevelApplicationBuilder

--- CS3733_D16_Idomeneus_Kabasuji/src/levelbuilder/view/TopLevelApplicationBuilder.java
+++ CS3733_D16_Idomeneus_Kabasuji/src/levelbuilder/view/TopLevelApplicationBuilder.java
@@ -7,6 +7,9 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import kabasuji.model.Builder;
+import kabasuji.model.Screen;
+import kabasuji.view.MainMenu;
+import kabasuji.view.TopLevelApplication;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -29,45 +32,29 @@ public class TopLevelApplicationBuilder extends JFrame {
 	}
 	public void changeContentPane(JPanel j){
 		contentPane.removeAll();
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
-				.addComponent(j, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 596, Short.MAX_VALUE)
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
-				.addComponent(j, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 287, Short.MAX_VALUE)
-		);
-		contentPane.setLayout(gl_contentPane);
+		j.setBounds(0,0,Screen.width,Screen.height);
+		contentPane.add(j);
 		contentPane.repaint();
 	}
 	void init(){
+		// puts top left corner into 10% of screen height and width
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		double width = screenSize.getWidth();
 		double height = screenSize.getHeight();
-		
 		int x0 = (int) (0.1*width);
-		int x1 = (int) (0.8*width);
 		int y0 = (int) (0.1*height);
-		int y1 = (int) (0.8*height);
-		
+		setBounds(x0,y0,Screen.width,Screen.height);
+		// not resizable
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(x0,y0,x1,y1);
+
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBounds(0, 0, Screen.width, Screen.height);
 		setContentPane(contentPane);
 		
 		BuilderMainMenu panel = new BuilderMainMenu(builder,TopLevelApplicationBuilder.this);
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
-				.addComponent(panel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 596, Short.MAX_VALUE)
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
-				.addComponent(panel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 287, Short.MAX_VALUE)
-		);
-		contentPane.setLayout(gl_contentPane);
+		panel.setBounds(0, 0, Screen.width, Screen.height);
+		contentPane.add(panel);
 	}
 }
 
