b4a4390400f3e91876d0733acd4e59ccbab7935c
odell <odelldotson@gmail.com>
2016-04-17 11:41:42 -0400

    Added test files.

---
+++ CS3733_D16_Idomeneus_Kabasuji/tests/kabasuji/controllerTests/GoToLevelSelectContollerTest.java
@@ -0,0 +1,22 @@
+package kabasuji.controllerTests;
+
+import kabasuji.*;
+import kabasuji.model.Kabasuji;
+import junit.framework.TestCase;
+
+public class GoToLevelSelectContollerTest extends TestCase{
+
+	protected void setUp()
+	{
+		//makeLevelsForTesting();
+
+		Kabasuji kabasuji = new Kabasuji();
+
+	}
+	
+	public void testTiles()
+	{
+
+
+	}
+}
---
+++ CS3733_D16_Idomeneus_Kabasuji/tests/kabasuji/modelTests/BuilderTest.java
@@ -0,0 +1,18 @@
+package kabasuji.modelTests;
+
+import kabasuji.model.*;
+import junit.framework.TestCase;
+
+public class BuilderTest extends TestCase{
+
+
+	protected void setUp()
+	{
+	}
+	
+	public void testTiles()
+	{
+
+
+	}
+}
