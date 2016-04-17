15f20e45f5bf37f8288f83a9f92b5290f8970f28
Vishal Rathi <vishalkumar95@users.noreply.github.com>
2016-04-16 22:37:19 -0400

    Add BuilderPuzzleLevelPanel and the background image for it

--- CS3733_D16_Idomeneus_Kabasuji/src/levelbuilder/view/BuilderPuzzleLevelPanel.java
+++ CS3733_D16_Idomeneus_Kabasuji/src/levelbuilder/view/BuilderPuzzleLevelPanel.java
@@ -1,126 +1,92 @@
 package levelbuilder.view;
 
-import javax.swing.border.CompoundBorder;
-
-import kabasuji.controller.GoToMainMenuController;
-import kabasuji.controller.SelectLevelController;
-import kabasuji.model.Builder;
-import kabasuji.model.Kabasuji;
-import levelbuilder.controller.GoToMainMenuBuilderController;
-
-import java.awt.Color;
-import java.awt.Font;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 
-import java.awt.SystemColor;
-import java.io.File;
+import kabasuji.model.Builder;
+import kabasuji.model.Screen;
+import kabasuji.view.JLabelIcon;
+import levelbuilder.controller.GoToMainMenuBuilderController;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class BuilderPuzzleLevelPanel extends JPanel {
 	Builder builder;
 	TopLevelApplicationBuilder app;
+
 	/**
-	 * Create the bullpen.
+	 * Create the panel.
 	 */
 	public BuilderPuzzleLevelPanel(Builder builder, TopLevelApplicationBuilder app) {
 		this.builder = builder;
 		this.app = app;
-		setSize(1000,800);
-		setBackground(SystemColor.textHighlight);
-		setBorder(new CompoundBorder());
-		setBackground(Color.WHITE);
-		
-		JButton btnNewButton_1 = new JButton("Delete Level");
-		btnNewButton_1.setBounds(830, 11, 150, 50);
-		btnNewButton_1.setBackground(Color.orange);
-		btnNewButton_1.setForeground(Color.BLACK);
-		btnNewButton_1.setContentAreaFilled(false);
-		btnNewButton_1.setOpaque(true);
-	
-		setLayout(null);
-		
-		add(btnNewButton_1);
-		
-		JButton btnNewButton_5 = new JButton("Undo");
-		btnNewButton_5.setBounds(830, 455, 150, 50);
-		btnNewButton_5.setBackground(Color.orange);
-		btnNewButton_5.setForeground(Color.BLACK);
-		btnNewButton_5.setContentAreaFilled(false);
-		btnNewButton_5.setOpaque(true);
-		
-		JButton btnNewButton_6 = new JButton("Redo");
-		btnNewButton_6.setBounds(830, 533, 150, 50);
-		btnNewButton_6.setBackground(Color.orange);
-		btnNewButton_6.setForeground(Color.BLACK);
-		btnNewButton_6.setContentAreaFilled(false);
-		btnNewButton_6.setOpaque(true);
-		
-		JButton btnNewButton_7 = new JButton("Save Level");
-		btnNewButton_7.setBounds(830, 613, 150, 50);
-		btnNewButton_7.setBackground(Color.orange);
-		btnNewButton_7.setForeground(Color.BLACK);
-		btnNewButton_7.setContentAreaFilled(false);
-		btnNewButton_7.setOpaque(true);
-		
-		JButton btnNewButton_8 = new JButton("Main Menu");
-		btnNewButton_8.setBounds(830, 215, 150, 50);
-		btnNewButton_8.setBackground(Color.orange);
-		btnNewButton_8.setForeground(Color.BLACK);
-		btnNewButton_8.setContentAreaFilled(false);
-		btnNewButton_8.setOpaque(true);
-		
-		//btnNewButton_8.addMouseListener(new GoToMainMenuBuilderController(builder, app, null));
-		//Added null because I had to change constructor for highlight color changes. - Odell
-		
-		JLabel movesLeft = new JLabel("Moves");
-		movesLeft.setBounds(0, 145, 150, 50);
-		movesLeft.setFont(new Font("Tahoma", Font.BOLD, 20));
-		movesLeft.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		JTextField movesLeftNum = new JTextField("12");
-		movesLeftNum.setBounds(0, 186, 150, 50);
-		movesLeftNum.setFont(new Font("Tahoma", Font.BOLD, 20));
-		movesLeftNum.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		add(btnNewButton_5);
-		add(btnNewButton_6);
-		add(btnNewButton_7);
-		add(btnNewButton_8);
-		add(movesLeft);
-		add(movesLeftNum);
-		
-		JPanel bullpen = new JPanel();
-		bullpen.setBounds(155, 450, 585, 350);
-		bullpen.setBackground(Color.lightGray);
-		add(bullpen);
-		bullpen.setLayout(null);
-		
-		JPanel board = new JPanel();
-		board.setBounds(196, 70, 500, 250);
-		board.setBackground(Color.WHITE);
-		add(board);
-		board.setLayout(null);
-		
-		String path = System.getProperty("user.dir") + File.separator + "src\\images\\tile.PNG";
-		String path_bullpen = System.getProperty("user.dir") + File.separator + "src\\images\\PiecesImage585x350.png";
 
-		for (int i = 0; i < 6; i++){
-			for (int j = 0; j < 6; j++){
-				ImageIcon image = new ImageIcon(path);
-				JLabel starlabel = new JLabel("", image, JLabel.CENTER);
-				starlabel.setBounds(j*50, i*50, 50, 50);
-				board.add(starlabel);
-			}
-		}
+		JLabelIcon background = new JLabelIcon("BuilderPuzzleLevel.png", Screen.width, Screen.height);
+		background.setBounds(0, 0, Screen.width, Screen.height);
+		add(background);
+
+		JLabelIcon bullpen = new JLabelIcon("boardpanel_opaque.png", (int) (Screen.width * 0.9),
+				(int) (Screen.height * 0.30));
+		bullpen.setLocation((int) (Screen.width * 0.05), (int) (Screen.height * 0.60));
+		background.add(bullpen);
 		
-		ImageIcon image = new ImageIcon(path_bullpen);
-		JLabel pieceLabel = new JLabel("", image, JLabel.CENTER);
-		pieceLabel.setBounds(0, 0, 585, 350);
-		bullpen.add(pieceLabel);
+		JLabelIcon board = new JLabelIcon("boardpanel_opaque.png", (int) (Screen.width * 0.5),
+				(int) (Screen.height * 0.5));
+		board.setLocation((int) (Screen.width * 0.22), (int) (Screen.height * 0.05));
+		background.add(board);
+
+		JLabelIcon undoBtn = new JLabelIcon("generalbutton.png", 70, 70);
+		undoBtn.setLocation((int) (Screen.width * 0.72) + (int) (undoBtn.getSize().getWidth() / 2),
+				(int) (Screen.height * 0.05));
+		JLabel undoLbl = new JLabel("<html>Undo</html>", SwingConstants.CENTER);
+		undoLbl.setBounds(0, 0, 70, 70);
+		undoLbl.setFont(new Font("Onyx", Font.BOLD, 18));
+		undoBtn.add(undoLbl);
+		background.add(undoBtn);
+
+		JLabelIcon redoBtn = new JLabelIcon("generalbutton.png", 70, 70);
+		redoBtn.setLocation((int) (Screen.width * 0.82) + (int) (undoBtn.getSize().getWidth() / 2),
+				(int) (Screen.height * 0.05));
+		JLabel redoLbl = new JLabel("<html>Redo</html>", SwingConstants.CENTER);
+		redoLbl.setBounds(0, 0, 70, 70);
+		redoLbl.setFont(new Font("Onyx", Font.BOLD, 18));
+		redoBtn.add(redoLbl);
+		background.add(redoBtn);
+
+		JLabelIcon saveBtn = new JLabelIcon("generalbutton.png", 70, 70);
+		saveBtn.setLocation((int) (Screen.width * 0.72) + (int) (saveBtn.getSize().getWidth() / 2),
+				(int) (Screen.height * 0.18));
+		JLabel saveLbl = new JLabel("<html>Save<br>Level</html>", SwingConstants.CENTER);
+		saveLbl.setBounds(0, 0, 70, 70);
+		saveLbl.setFont(new Font("Onyx", Font.BOLD, 18));
+		saveBtn.add(saveLbl);
+		background.add(saveBtn);
+
+		JLabelIcon deleteBtn = new JLabelIcon("generalbutton.png", 70, 70);
+		deleteBtn.setLocation((int) (Screen.width * 0.82) + (int) (saveBtn.getSize().getWidth() / 2),
+				(int) (Screen.height * 0.18));
+		JLabel deleteLbl = new JLabel("<html>Delete<br>Level</html>", SwingConstants.CENTER);
+		deleteLbl.setBounds(0, 0, 70, 70);
+		deleteLbl.setFont(new Font("Onyx", Font.BOLD, 18));
+		deleteBtn.add(deleteLbl);
+		background.add(deleteBtn);
+		
+		JTextField numMoves = new JTextField();
+		numMoves.setBounds((int) (Screen.width * 0.08 + 10), (int) (Screen.height * 0.30), 40, 20);
+		numMoves.setFont(new Font("Onyx", Font.BOLD, 18));
+		background.add(numMoves);
+
+		JLabelIcon mainmenubtn = new JLabelIcon("generalbutton.png", 70, 70);
+		mainmenubtn.setLocation((int) (Screen.width * 0.78) + (int) (saveBtn.getSize().getWidth() / 2),
+				(int) (Screen.height * .35));
+		JLabel mainmenulbl = new JLabel("<html>Main<br>Menu</html>", SwingConstants.CENTER);
+		mainmenulbl.setBounds(0, 0, 70, 70);
+		mainmenulbl.setFont(new Font("Onyx", Font.BOLD, 18));
+		mainmenubtn.add(mainmenulbl);
+		mainmenubtn.addMouseListener(
+				new GoToMainMenuBuilderController(builder, app,mainmenubtn));
+		background.add(mainmenubtn);
 	}
 }
