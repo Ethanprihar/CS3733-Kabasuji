e0e48da2bad28413e9898b974c97d8d3effd2601
Ethanprihar <ethanprihar@gmail.com>
2016-04-27 11:56:22 -0400

    added controller test archatecture

---
+++ CS3733_D16_Idomeneus_Kabasuji/tests/kabasuji/controllerTests/TestBuilder.java
@@ -0,0 +1,91 @@
+package kabasuji.controllerTests;
+
+import kabasuji.testUtilities.TestCaseHelper;
+
+public class TestBuilder extends TestCaseHelper
+{
+	public void setup()
+	{
+		
+	}
+	
+	public void testBuilderBoard()
+	{
+		
+	}
+	
+	public void testBuilderReleaseBoard()
+	{
+		
+	}
+	
+	public void testCreateNewLevelBuilder()
+	{
+		
+	}
+	
+	public void testDeleteLevel()
+	{
+		
+	}
+	
+	public void testDeselectReleaseButtons()
+	{
+		
+	}
+	
+	public void testEditNewLevelBuilder()
+	{
+		
+	}
+	
+	public void testGoToMainMenuBuilder()
+	{
+		
+	}
+	
+	public void testIncrementPieceBuilder()
+	{
+		
+	}
+	
+	public void testLoadLevelsBuilder()
+	{
+		
+	}
+	
+	public void testRedo()
+	{
+		
+	}
+	
+	public void testSaveLevel()
+	{
+		
+	}
+	
+	public void testSelectLevelBuilder()
+	{
+		
+	}
+	
+	public void testSetColor()
+	{
+		
+	}
+	
+	public void testSetNumber()
+	{
+		
+	}
+	
+	public void testTestLevelBuilder()
+	{
+		
+	}
+	
+	public void testUndo()
+	{
+		
+	}
+}
---
+++ CS3733_D16_Idomeneus_Kabasuji/tests/kabasuji/controllerTests/TestKabasuji.java
@@ -0,0 +1,61 @@
+package kabasuji.controllerTests;
+
+import kabasuji.testUtilities.TestCaseHelper;
+
+public class TestKabasuji extends TestCaseHelper
+{
+	public void setup()
+	{
+		
+	}
+	
+	public void testBoard()
+	{
+		
+	}
+	
+	public void testFlipSelectedPieceBullpen()
+	{
+		
+	}
+	
+	public void testGoToLevelSelect()
+	{
+		
+	}
+	
+	public void testGoToMainMenu()
+	{
+		
+	}
+	
+	public void testNextLevel()
+	{
+		
+	}
+	
+	public void testResetLevel()
+	{
+		
+	}
+	
+	public void testRotateSelectedPieceBullpen()
+	{
+		
+	}
+	
+	public void testSelectLevel()
+	{
+		
+	}
+	
+	public void testSelectPieceBullpen()
+	{
+		
+	}
+	
+	public void testTile()
+	{
+		
+	}
+}
